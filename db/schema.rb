1497fc810732f3056108fa6d1c3322efd5b23349
derfman9303 <frederichodges@gmail.com>
2020-03-18 21:59:54 +0000

    styled card

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_014333) do
+ActiveRecord::Schema.define(version: 2020_03_18_190635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "front"
+    t.string "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "faqs", force: :cascade do |t|
     t.string "question"
